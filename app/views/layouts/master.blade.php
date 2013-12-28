d824c55ddda3857a4738f843e01ec1158471fb61
Daniel Twigg <dtwigg1990@gmail.com>
2013-12-28 20:29:52 +0000

    added Foundation files added CSS and JS to master view

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -1,4 +1,21 @@
-<html>
+<!DOCTYPE html>
+<!--[if IE 9]><html class="lt-ie10" lang="en" > <![endif]-->
+<html class="no-js" lang="en" >
+
+<head>
+	<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  	<link rel="stylesheet" type="text/css" href="{{ asset('css/normalize.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/foundation-min.css') }}">
+
+	<script src="{{ asset('js/vendor/jquery.js') }}"></script>
+	<script src="{{ asset('js/vendor/foundation-min.js') }}"></script>
+	<script>
+    $(document).foundation();
+  </script>
+</head>
+
     <body>
     	<header>
     		@if ( ! Sentry::check() )
