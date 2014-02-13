a287ed77183b656337197eeea97a18d44505f83e
Daniel Twigg <dtwigg1990@gmail.com>
2014-02-13 21:30:01 +0000

    Revert "Update database.php"
    This reverts commit 7181b1bd2737ca2218752d7a198ab94b0b7a9f1a.

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => '',
-			'username'  => '',
-			'password'  => '',
+			'database'  => 'zadmin_rent',
+			'username'  => 'admin',
+			'password'  => 'yru7eseta',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
