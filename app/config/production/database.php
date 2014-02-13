7181b1bd2737ca2218752d7a198ab94b0b7a9f1a
djam90 <dtwigg1990@gmail.com>
2014-02-13 21:19:46 +0000

    Update database.php
    removed my public config

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'zadmin_rent',
-			'username'  => 'admin',
-			'password'  => 'yru7eseta',
+			'database'  => '',
+			'username'  => '',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
