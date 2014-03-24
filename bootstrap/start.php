1e3f4bd56d033137317eb6f7dedaf6c8ced4e383
Daniel Twigg <daniel.twigg@sandicliffe.co.uk>
2014-03-24 16:15:59 +0000

    Modified the local hosts

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('dan-win8'),
+	'local' => array('dan-win8',"STAINT06"),
 	'production' => array('danieltwigg')
 ));
 
